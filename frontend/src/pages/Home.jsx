28a38fa385af9626f1e1049269e5dcc0a16dbd5c
Mohanraj111789 <kit27.am30@gmail.com>
2025-12-29 14:12:02 +0530

    frontend setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+
+  return (
+    <div>
+      <h1>Welcome to Mohan E-Shop</h1>
+      <p>Explore our products and make your shopping experience seamless.</p>
+    </div>
+  );
+}
