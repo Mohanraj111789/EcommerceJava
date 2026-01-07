678b0479467b80a8d398683bd34fbe0c84cc3b49
Mohanraj111789 <kit27.am30@gmail.com>
2026-01-07 14:08:29 +0530

    Add the empty product image in the product page

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,34 +4,35 @@ import { useNavigate } from "react-router-dom";
 
 const Card = ({ product, onAddToCart, onBuyNow }) => {
   const navigate = useNavigate();
+
   const handleClick = () => {
-    navigate(`/product/${product.id}`,
-      {state: {product}
-    });
+    navigate(`/product/${product.id}`, { state: { product } });
   };
-  const BASE_URL = `../public/assets/`;
+
   return (
-    <div className="product-card" >
-      {/*i want to add product image here */}
+    <div className="product-card">
       <div className="product-image-container" onClick={handleClick}>
-        <img src= {`../assets/${product.imageUrl}`} className="product-image"></img>
+        <img
+          src={
+            product.imageUrl
+              ? `/assets/${product.imageUrl}`
+              : `/assets/product.jpg`
+          }
+          className="product-image"
+          alt={product.name}
+        />
       </div>
+
       <h4 className="product-name">{product.name}</h4>
       <p className="product-price">₹ {product.price}</p>
       <p className="product-description">{product.description}</p>
-      <strong className="product-category">Stock:{product.stock}</strong>
+      <strong className="product-category">Stock: {product.stock}</strong>
 
-      <button
-        className="btn add-btn"
-        onClick={() => onAddToCart(product.id)}
-      >
+      <button className="btn add-btn" onClick={() => onAddToCart(product.id)}>
         Add to Cart
       </button>
 
-      <button
-        className="btn buy-btn"
-        onClick={() => onBuyNow(product.id)}
-      >
+      <button className="btn buy-btn" onClick={() => onBuyNow(product.id)}>
         Buy Now
       </button>
     </div>
