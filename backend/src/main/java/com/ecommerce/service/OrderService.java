25a328cc505c667ace186c9c10f28d123ea6bf70
Mohanraj111789 <kit27.am30@gmail.com>
2026-01-11 20:47:38 +0530

    OrderDervice creation

---
+++ backend/src/main/java/com/ecommerce/service/OrderService.java
@@ -0,0 +1,38 @@
+package com.ecommerce.service;
+
+import com.ecommerce.model.Order;
+import com.ecommerce.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    private final OrderRepository repo;
+
+    public OrderService(OrderRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Order> getAll() {
+        return repo.findAll();
+    }
+
+    public Optional<Order> getById(Long id) {
+        return repo.findById(id);
+    }
+
+    public Order save(Order order) {
+        return repo.save(order);
+    }
+
+    public void deleteById(Long id) {
+        repo.deleteById(id);
+    }
+
+
+
+
+
+    
+}
