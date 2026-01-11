8aad83e22f5f5d42beab0b9e87c486860df2cb38
Mohanraj111789 <kit27.am30@gmail.com>
2026-01-11 20:20:36 +0530

    OrderRepository creation

---
+++ backend/src/main/java/com/ecommerce/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long>{
+
+
+    
+}
