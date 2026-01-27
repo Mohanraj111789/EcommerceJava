55e1cbdbc0c715082600068d0d10d231cde92119
Mohanraj111789 <kit27.am30@gmail.com>
2026-01-27 22:18:25 +0530

    Test finished 27 jan 10:18

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,5 @@
-import API from "../config/api";
 
-
-
-const API_URL = import.meta.env.API_URL; // Update with your backend URL
+const API_URL = 'https://ecommercejava-2.onrender.com/api'; // Update with your backend URL
 
 
 
