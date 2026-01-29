d0d696963f7453a1564ca2deaa308aa70f3d6730
Mohanraj111789 <kit27.am30@gmail.com>
2026-01-30 00:34:00 +0530

    Fix CORS issue in backend

--- backend/src/main/java/com/ecommerce/security/SecurityConfig.java
+++ backend/src/main/java/com/ecommerce/security/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://localhost:5174",""));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://localhost:5174","https://ecommercejava-2.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
