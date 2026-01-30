78eedd7a4c04ee39146f65373ed530272078b897
Mohanraj111789 <kit27.am30@gmail.com>
2026-01-30 07:42:20 +0530

    Give the Cors headers

--- backend/src/main/java/com/ecommerce/security/SecurityConfig.java
+++ backend/src/main/java/com/ecommerce/security/SecurityConfig.java
@@ -51,7 +51,11 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://localhost:5174","https://ecommercejava-2.onrender.com","https://ecompcart.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList(
+            "Authorization",
+            "Content-Type",
+            "Idempotency-Key"
+        ));
         configuration.setAllowCredentials(true);
         
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
